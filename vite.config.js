c206801f846864b141fbed1f9792d61b8e2792f9
Angie719-web <angelicavaati@gmail.com>
2025-10-19 12:47:42 +0300

    Tailwind Card

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/react-js-jsx-and-css-mastering-front-end-development-Angie719-web/',
   plugins: [react()],
   server: {
     port: 5173, // always use this port
